45bee3caae9073d587253eee96d6f0292f626bd1
NikolaMarinkovikj <nikolamarinkovikj7@gmail.com>
2024-02-21 17:40:59 +0100

    wordpress

---
+++ wp-content/themes/carclubmk/front-page.php
@@ -0,0 +1,13 @@
+<?php
+
+get_header();
+
+?>
+    <main class="main">
+        <?php
+        get_template_part('sections/beginner','v1')
+        ?>
+    </main>
+<?php
+
+get_footer();
---
+++ wp-content/themes/carclubmk/sections/beginner-v1.php
@@ -0,0 +1,105 @@
+<?php
+    $cards = array(
+        array(
+            'title' => 'Audi',
+            'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+        ),
+        array(
+            'title' => 'Chevrolet',
+            'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+        ),
+        array(
+            'title' => 'Ford',
+            'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+        ),
+        array(
+            'title' => 'Golf',
+            'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+        ),
+    );
+
+?>
+
+<section class="s-begginer">
+    <div class="s-begginer__inner container">
+        <div class="c-headline">
+            <div class="c-headline_before">
+                START YOUR CARS JOURNEY
+            </div>
+            <h2 class="c-headline_main">
+                Beginner's Corner
+            </h2>
+            <div class="c-headline_after">
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+        </div>
+
+        <div class="c-cards">
+            <div class="c-cards_inner">
+                <div class="c-cards_card">
+                    <div class="c-cards_header">
+                        <h1>Golf 4</h1>
+                    </div>
+                    <div class="c-cards_body">
+                        <p>The Volkswagen Golf 4 GTI is often regarded as one of the best iterations of the iconic GTI lineup. Launched in the late 1990s and produced until the mid-2000s, the Golf 4 GTI perfectly encapsulates the essence of what enthusiasts love about hot hatches: a perfect blend of performance, practicality, and driving enjoyment. </p>
+                    </div>  
+                    <div class="c-cards_icon">
+                    <img src="https://images.ctfassets.net/uaddx06iwzdz/2RPTl0s1ztYbNj36T5bE5Y/3a98718ef6669326ee5ac3c77c1b14f1/AS24-modelpagina-vw-golf-4_12.jpeg" alt="Golf_5">
+                    </div>
+                    <div class="c-cards_title">
+                        
+                    </div>
+                    <div class="c-cards_desc">
+                        
+                    </div>
+                </div>
+            </div>
+
+
+        <div class="c-cards">
+            <div class="c-cards_inner">
+                <div class="c-cards_card">
+                    <div class="c-cards_header">
+                        <h1>Golf 5</h1>
+                    </div>
+                    <div class="c-cards_body">
+                        <p>The Volkswagen Golf 5 GTI, introduced in the mid-2000s, continued the legacy of the GTI lineup with its blend of performance, refinement, and practicality. Here's why the Golf 5 GTI is often celebrated as another standout model in the GTI family</p>
+                    </div>
+                    <div class="c-cards_icon">
+                        <img src="https://cdn.motor1.com/images/mgl/9mm9bX/s1/volkswagen-golf-v-r32-2005.jpg" alt="Golf_5">
+                    </div>
+                    <div class="c-cards_title">
+                        
+                    </div>
+                    <div class="c-cards_desc">
+                        
+                    </div>
+                </div>
+            </div>
+
+
+        <div class="c-cards">
+            <div class="c-cards_inner">
+                <div class="c-cards_card">
+                    <div class="c-cards_header">
+                    <h1>Golf 6</h1>
+                    </div>
+                    <div class="c-cards_body">
+                        <p>The Volkswagen Golf 6 GTI, introduced in the late 2000s, further refined the iconic GTI formula, offering enhanced performance, comfort, and technology. Here's why the Golf 6 GTI is revered as another significant iteration in the GTI lineage</p>
+                    </div>
+                    <div class="c-cards_icon">
+                    <img src="https://media.carsandbids.com/cdn-cgi/image/width=2080,quality=70/d9b636c2ec84ddc3bc7f2eb32861b39bdd5f9683/photos/9AYWY7lw-TkvMdZxII0-(edit).jpg?t=165595219077" alt="Golf_5">    
+                    </div>
+                    <div class="c-cards_title">
+                        
+                    </div>
+                    <div class="c-cards_desc">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</section>
