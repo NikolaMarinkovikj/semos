e491f69997939012cc57f865fb08ee5345d474fd
NikolaMarinkovikj <nikolamarinkovikj7@gmail.com>
2024-02-28 16:08:27 +0100

    nesto

--- wp-content/themes/carclubmk/front-page.php
+++ wp-content/themes/carclubmk/front-page.php
@@ -7,6 +7,35 @@ get_header();
         <?php
         get_template_part('sections/beginner','v1')
         ?>
+
+        <?php
+
+            $people = array (
+                array(
+                    "first_name" => "Nikola"
+                    "fast_name" => "Marinkovikj"
+                    array(
+                    "Age" => 19,
+                    "Height" => 175,
+                    "Weight" => 80,
+                    )
+                ),
+                array(
+                    "first_name" => "Ruzhica"
+                    "fast_name" => "Zhekoska"
+                    array(
+                    "Age" => 19,
+                    "Height" => 175,
+                    "Weight" => 80,
+                    )
+                ),
+            );
+        ?>
+
+            <div>
+            Ruzhica e postara od Nikola
+            </div>
+
     </main>
 <?php
 
